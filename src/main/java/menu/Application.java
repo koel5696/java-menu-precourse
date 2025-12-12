e38a88ece41125d6f1bcb88737756ac569ddad09
윤민수 <koelysa5696@naver.com>
2025-12-12 18:53:22 +0900

    feat: 랜덤 관련 의존성 주입

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -2,6 +2,8 @@ package menu;
 
 
 import menu.controller.MenuController;
+import menu.domain.util.CategoryNumberGenerator;
+import menu.domain.util.MenuShuffle;
 import menu.service.MenuRecommendService;
 import menu.view.InputView;
 import menu.view.OutputView;
@@ -9,7 +11,12 @@ import menu.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         MenuController menuController = new MenuController(
-                new OutputView(), new InputView(), new MenuRecommendService()
+                new OutputView(),
+                new InputView(),
+                new MenuRecommendService(
+                        new CategoryNumberGenerator(),
+                        new MenuShuffle()
+                )
         );
         menuController.run();
     }

8dd2eb5f2bfbc02e0f641a4c8ba56e893066b2c9
윤민수 <koelysa5696@naver.com>
2025-12-12 16:45:30 +0900

    feat: 사용자 입력 ui 구현
    - 코치 이름 입력
    - 먹지 못하는 메뉴 입력

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -3,13 +3,13 @@ package menu.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String getCoachNames() {
+    public String inputCoachNames() {
         System.out.println();
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         return Console.readLine();
     }
 
-    public String getHateMenus(String name) {
+    public String inputUneatableMenu(String name) {
         System.out.println();
         System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         return Console.readLine();

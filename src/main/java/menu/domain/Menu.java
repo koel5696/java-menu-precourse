7b8b83e4b35492367292f40207ebf1843f3f0664
윤민수 <koelysa5696@naver.com>
2025-12-12 16:55:40 +0900

    feat: 메뉴 규칙 구현

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,55 @@
+package menu.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import menu.domain.util.RandomUtil;
+
+public enum Menu {
+    JAPAN(1, "일식", "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼"),
+    KOREA(2, "한식", "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음"),
+    CHINESE(3, "중식", "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채"),
+    ASIAN(4, "아시안", "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜"),
+    WESTERN(5, "양식", "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니");
+
+    private final int categoryNumber;
+    private final String category;
+    private final List<String> menus;
+
+    Menu(int categoryNumber, String category, String menus) {
+        this.categoryNumber = categoryNumber;
+        this.category = category;
+        this.menus = Arrays.stream(menus.split(","))
+                .map(String::trim)
+                .toList();
+    }
+
+    public static void validateMenu(String inputMenu) {
+        if (inputMenu.isEmpty()) {
+            return;
+        }
+
+        boolean exist = Arrays.stream(Menu.values())
+                .anyMatch(category -> category.menus.contains(inputMenu));
+
+        if (!exist) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다.");
+        }
+    }
+
+    public static Menu findCategory(int categoryNumber) {
+        for (Menu menu : Menu.values()) {
+            if (menu.categoryNumber == categoryNumber) {
+                return menu;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 카테고리 번호입니다.");
+    }
+
+    public String getMenu() {
+        return RandomUtil.menuShuffle(menus);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
